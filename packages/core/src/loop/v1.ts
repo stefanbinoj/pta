58722690af3d1052429615d24a0abbf8dec7c57a
stefan binoj <stefan.binoj.007@gmail.com>
2025-09-24 18:45:42 +0530

    agent in the loop done

--- packages/core/src/loop/v1.ts
+++ packages/core/src/loop/v1.ts
@@ -1,16 +1,50 @@
 import OpenAI from "openai";
 import { schema, systemPrompt } from "./loop_system_prompt.ts";
+import type { ChatCompletionMessageParam } from "openai/resources/chat";
+
 const client = new OpenAI();
+const messages : ChatCompletionMessageParam[] = [
+    { role: "system", content: systemPrompt },
+    { role: "user", content: "what is my tasks for today?" },
+]
 
 export const main = async () => {
-    const completion = await client.chat.completions.create({
-        model: "gpt-5-2025-08-07",
-        messages: [
-            { role: "system", content: systemPrompt },
-            { role: "user", content: "what is my tasks for today?" },
-        ],
-        response_format: { type: "json_schema" , json_schema: schema ,},
-    });
-    console.log(completion.choices[0].message);
+    while (true) {
+        const completion = await client.chat.completions.create({
+            model: "gpt-5-2025-08-07",
+            messages: messages,
+            response_format: { type: "json_schema", json_schema: schema, },
+        });
+        const response = completion.choices[0].message
+        messages.push({role: "assistant", content: response.content as string});
+
+        const content = JSON.parse(response.content as string);
+        const step = content.step;
+        const aiContent = content.content;
+        console.log(step);
+        console.log(aiContent);
+
+        if(step === "analyze"){
+            // do nothing, wait for next input
+        }
+        else if(step === "think"){
+            // do nothing, wait for next input
+        }
+        else if(step === "tool_call"){
+        }
+        else if(step === "observe"){
+            // do nothing, wait for next input
+        }
+        else if(step === "continue"){
+            // do nothing, wait for next input
+        }
+        else if(step === "result"){
+            console.log("Final Result: ", aiContent);
+            break;
+        }
+
+
+    }
+
 };
 
